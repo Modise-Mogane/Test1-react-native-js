30d4b676cfb764a3fb056312e467f5deeaf36ea9
Tshepiso-333 <modisemogane@gmail.com>
2025-08-27 12:41:35 +0200

    question2

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -6,6 +6,7 @@ export default function HomeScreen({ navigation }) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
